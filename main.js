7994b7af0ee857b64bd72cd29b279838814ae881
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-09-29 19:22:56 +0000

    HTML is generating!!!

--- main.js
+++ main.js
@@ -1,80 +1,51 @@
 
-/*✅ FUNCTION: getWeather (populates elements w/ API data) *//////
-function getWeather(ZIPcode) {
+/////////* GENERATES HTML: why are my cards weird?????????? */////////
+const main = document.getElementById('main');
 
-const APIurl = `https://api.openweathermap.org/data/2.5/weather?zip=${ZIPcode},us&appid=64a10bf79d7c527df3631aa5317b84ed&units=imperial`;
-/* ?????????????????create function to change the units to metric/standard, maybe add toggle button? 
-/* ?????????????????use template literals on units in url *////
-
-axios.get(APIurl)
-
-.then(function (response) {
-  console.log(response);
-
-  document.getElementById("nameDisplay").innerText = `${response.data.name}`;
-  nameDisplay.style.color = "black";
-  console.log(response.data.name);
-
-  document.getElementById("tempDisplay").innerText = `${response.data.main.temp}°F`;
-  console.log(response.data.main.temp);
-  
-  document.getElementById("descDisplay").innerText = `${response.data.weather[0].description}`;
-  console.log(response.data.weather[0].description);
-
-})
-
-.catch(function (error) {
-  document.getElementById("nameDisplay").innerText = "Oops. Please input a valid ZIP code.";
-  nameDisplay.style.color = "red";
-  console.log(error);
-})
-
-.then(function () {
-  console.log("Finished");
-});
+/*✅ */function createElement (parent, idX, classX, tag, text, type, style, source) {
+  let element = document.createElement(tag);
+  console.log(element);
+  element.setAttribute('id', idX);
+  element.className = classX;
+  element.innerText = text;
+  element.setAttribute('type', type);
+  element.setAttribute('style', style)
+  element.setAttribute('src', source)
+  parent.appendChild(element);
+  console.log(element);
 }
 
-/* ✅ FUNCTION: onclick (receives input from text field to activate getWeather on button click) *//////
-window.onload = function() {
-  document.getElementById("searchButton").onclick = function(){
-    const ZIPcode = document.getElementById("ZIPinput").value;
-    getWeather(ZIPcode);
-  }
+createElement(main, 'pargraphID', 'pinktext', 'p', 'hello'); 
+
+///making the HEADER and text input/////
+/*✅*/function generateHeader (){
+createElement(main, 'headerDiv', 'headerDiv', 'div', null, null, null, null); 
+createElement(main, null, null, 'h1', 'Weather App', null, null, null); 
+createElement(main, 'ZIPinput', null, 'input', null, 'text', null, null); 
+createElement(main, 'searchButton', null, 'button', 'Search', null, null, null); 
 }
+generateHeader();
 
+/*🛑*/function generateCards() {
+//making CITY NAME card appear//
+createElement(main, null, 'card text-center', 'div', null, null, 'width: 18rem', null);
+createElement(main, null, 'card-body', 'div', null, null, null, null);
+createElement(main, null, 'card-title', 'h5', 'city', null, null, null);
+createElement(main, 'nameDisplay', 'p', null, null, null, null, null);
 
-function makeThis (tag, type, idName, src, classX, text, parent) {
-  tag = document.createElement(tag);
-    type = tag.setAttribute('type', type)
-    idName = tag.setAttribute('id', idName);
-    parent.appendchild(tag);
-};
+//making TEMPERATURE card appear//
+createElement(main, null, 'card text-center', 'div', null, null, 'width: 18rem', null);
+createElement(main, null, 'card-body', 'div', null, null, null, null);
+createElement(main, null, 'card-title', 'h5', 'temperature', null, null, null);
+createElement(main, 'tempDisplay', 'p', null, null, null, null, null);
 
-
-document.createElement("button");
-    resetbutton.innerHTML = "reset";
-    resetbutton.className = "resetbutton";
-    resetbutton.setAttribute('id', 'resetbutton'); /*add ID to be used in toggleButtons function*/
-    resetbutton.setAttribute('title', 'imbibe tincture of amnesia');
-    document.body.appendChild(resetbutton);
-
-
-
-
-function generatePage (){
-  window.body = document.querySelector('body');
-  makeThis('h1', null, 'header', null, 'container-fluid text-primary', body, 'please work');
+//making WEATHER CONDITION card appear//
+createElement(main, null, 'card text-center', 'div', null, null, 'width: 18rem', null);
+createElement(main, null, 'card-body', 'div', null, null, null, null);
+createElement(main, null, 'card-title', 'h5', 'weather', null, null, null);
+createElement(main, 'descDisplay', 'p', null, null, null, null, null);
+createElement(main, null, 'card-img-bottom', 'img', null, null, null, 'images/moonbeam.png');
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
