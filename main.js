88977f3a5a6be01e096cdaaef3d9514741ddebf8
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-09-29 19:28:57 +0000

    SHE WORKS!!!!! HTML and API data populate on click

--- main.js
+++ main.js
@@ -48,6 +48,53 @@ createElement(main, null, 'card-img-bottom', 'img', null, null, null, 'images/mo
 }
 
 
+/////////GETS API DATA////////
+
+/*✅*/function getWeather(ZIPcode) {
+
+  const APIurl = `https://api.openweathermap.org/data/2.5/weather?zip=${ZIPcode},us&appid=64a10bf79d7c527df3631aa5317b84ed&units=imperial`;
+  /* ?????????????????create function to change the units to metric/standard, maybe add toggle button? 
+  /* ?????????????????use template literals on units in url *////
+  
+  axios.get(APIurl)
+  
+  .then(function (response) {
+    console.log(response);
+  
+    document.getElementById("nameDisplay").innerText = `${response.data.name}`;
+    nameDisplay.style.color = "black";
+    console.log(response.data.name);
+  
+    document.getElementById("tempDisplay").innerText = `${response.data.main.temp}°F`;
+    console.log(response.data.main.temp);
+    
+    document.getElementById("descDisplay").innerText = `${response.data.weather[0].description}`;
+    console.log(response.data.weather[0].description);
+  })
+  
+  .catch(function (error) {
+    document.getElementById("nameDisplay").innerText = "Oops. Please input a valid ZIP code.";
+    nameDisplay.style.color = "red";
+    console.log(error);
+  })
+  
+  .then(function () {
+    console.log("Finished");
+  });
+  }
+  
+  
+  /*✅*/ window.onload = function() {
+    document.getElementById("searchButton").onclick = function(){
+      const ZIPcode = document.getElementById("ZIPinput").value;
+      getWeather(ZIPcode);
+      generateCards();
+    }
+  }
+  
+  
+
+
 
 
 
